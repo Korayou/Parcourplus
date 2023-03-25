6b0129987c1acb747bbf62c88e1120b585b2ecf7
horville <ewen.horville@etu.u-pec.fr>
2023-03-25 20:29:49 +0100

    Structuration

---
+++ javascript/school-info.js
@@ -0,0 +1,10 @@
+var schoolInfo = {
+  css: null,
+  exports: {
+    onMounted() {}
+  },
+  template: (template, expressionTypes, bindingTypes, getComponent) => template('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.3/css/bulma.min.css"/><div><div></div></div>', []),
+  name: 'school-info'
+};
+
+export { schoolInfo as default };
